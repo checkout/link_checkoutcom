9f3116fe123fb91bbc97d5ab05e468dfa8b0050a
michael-ugborioko-cko <michael.ugborikoko@checkout.com>
2020-11-11 16:50:20 +0000

    fixed guest user card payement issue

--- Cartridges/int_checkoutcom_sfra/cartridge/scripts/hooks/payment/processor/CHECKOUTCOM_CARD.js
+++ Cartridges/int_checkoutcom_sfra/cartridge/scripts/hooks/payment/processor/CHECKOUTCOM_CARD.js
@@ -183,7 +183,7 @@ function Handle(basket, paymentInformation, paymentMethodID, req) {
             securityCode: cardSecurityCode,
             storedPaymentUUID: paymentInformation.storedPaymentUUID,
             saveCard: paymentInformation.saveCard.value,
-            customerNo: req.currentCustomer.profile.customerNo,
+            customerNo: req.currentCustomer.raw.registered ? req.currentCustomer.profile.customerNo : null ,
         });
     });
 
