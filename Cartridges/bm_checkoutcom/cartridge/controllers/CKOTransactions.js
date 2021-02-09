818078d71e6cd4414c610c70ecf5e2f0ce6528bf
michael-ugborikoko-cko <michael.ugborikoko@checkout.com>
2021-02-09 22:59:18 +0000

    ECPINT-1988 BM-Klarna Capture, Refund and Void fix

--- Cartridges/bm_checkoutcom/cartridge/controllers/CKOTransactions.js
+++ Cartridges/bm_checkoutcom/cartridge/controllers/CKOTransactions.js
@@ -2,6 +2,8 @@
 
 /* API Includes */
 var ISML = require('dw/template/ISML');
+var OrderMgr = require('dw/order/OrderMgr');
+var Site = require('dw/system/Site');
 
 /* Checkout.com Helper functions */
 var CKOHelper = require('~/cartridge/scripts/helpers/CKOHelper');
@@ -37,8 +39,17 @@ function remoteCall() {
     // eslint-disable-next-line
     var task = request.httpParameterMap.get('task');
 
+    // Get the order number
+    var orderNumber = request.httpParameterMap.get('orderNo');
+
     // Get the transaction currency
     var currency = request.httpParameterMap.get('currency');
+
+    // Load the order
+    var order = OrderMgr.getOrder(orderNumber);
+
+    // Get the payment method
+    var paymentMethod = order.getPaymentInstrument().getPaymentMethod();
 
     // Get the transaction formated amount
     var formatedAmount = CKOHelper.getFormattedPrice(request.httpParameterMap.get('amount').stringValue, currency);
@@ -52,6 +63,41 @@ function remoteCall() {
 
     // Set the service parameter
     var serviceName = 'cko.transaction.' + task + '.' + mode + '.service';
+
+    // Capture and Void Klarna Transactions
+    if (paymentMethod === 'KLARNA') {
+
+        // eslint-disable-next-line
+        if (task.value === 'void' || task.value === 'refund') {
+            if (task.value === 'void') {
+                gRequest = {
+                    chargeId: request.httpParameterMap.get('pid').stringValue, // eslint-disable-next-line
+                    reference: orderNumber.value,
+                }
+            } else {
+                gRequest = {
+                    amount: formatedAmount, // eslint-disable-next-line
+                    chargeId: request.httpParameterMap.get('pid').stringValue, // eslint-disable-next-line
+                    reference: orderNumber.value,
+                }
+            }
+        } else {
+            gRequest = {
+                amount: formatedAmount, // eslint-disable-next-line
+                chargeId: request.httpParameterMap.get('pid').stringValue, // eslint-disable-next-line
+                reference: orderNumber.value,
+                type: "klarna",
+                klarna: {
+                    description: CKOHelper.getValue('ckoBusinessName') !== '' && CKOHelper.getValue('ckoBusinessName') !== 'undefined' 
+                        ? CKOHelper.getValue('ckoBusinessName') : Site.getCurrent().httpHostName
+                }
+            }
+        }
+
+        if (task.value !== 'refund') {
+            serviceName = 'cko.klarna_transaction.' + task + '.' + mode + '.service';
+        }
+    };
 
     // Log the payment request data
     CKOHelper.log(
