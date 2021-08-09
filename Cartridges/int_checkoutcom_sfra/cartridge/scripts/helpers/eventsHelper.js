0aad9fc4d2fbb4d2fd2fb7f6f4305382a66b7736
Alin Anghelus (Endava) <alin.anghelus@endava.com>
2021-08-09 11:33:26 +0300

    comment changes

--- Cartridges/int_checkoutcom_sfra/cartridge/scripts/helpers/eventsHelper.js
+++ Cartridges/int_checkoutcom_sfra/cartridge/scripts/helpers/eventsHelper.js
@@ -84,7 +84,7 @@ var eventsHelper = {
                 // Update order if flagged
                 if (flagDetails) {
                     order.addNote(ckoHelper._('cko.webhook.info', 'cko'), flagDetails);
-                    order.setConfirmationStatus(0);
+                    order.setConfirmationStatus(order.CONFIRMATION_STATUS_NOTCONFIRMED);
                 }
 
                 // Update the order status
@@ -126,7 +126,7 @@ var eventsHelper = {
             paymentInstrument.paymentTransaction.setType(PaymentTransaction.TYPE_CAPTURE);
 
             setPaymentStatus(order);
-            order.setConfirmationStatus(2);
+            order.setConfirmationStatus(order.CONFIRMATION_STATUS_CONFIRMED);
 
             // Update the parent transaction state
             var parentTransaction = transactionHelper.getParentTransaction(hook, 'Authorization');
